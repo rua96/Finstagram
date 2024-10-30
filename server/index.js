6ffa1721f2d53b794513d60b2eb5a39173b2952e
rua96 <rua96@hotmail.it>
2024-10-30 11:18:07 +0100

    ewjrieit2

--- server/index.js
+++ server/index.js
@@ -1,27 +1,26 @@
 const express = require("express");
-const app=express();
-const cors =require ('cors');
+const app = express();
+const cors = require("cors");
 
-const db=require('./models');
+const db = require("./models");
 
-require('dotenv').config()
+require("dotenv").config();
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
+const postsRouter = require("./routes/posts");
+app.use("/posts", postsRouter);
+const postsLikesRouter = require("./routes/postsLikes");
+app.use("/postsLikes", postsLikesRouter);
+const postsCommentsRouter = require("./routes/postsComments");
+app.use("/postsComments", postsCommentsRouter);
+const imagesRouter = require("./routes/images");
+app.use("/images", imagesRouter);
 
-const usersRouter =require('./routes/users');
-app.use('/users',usersRouter);
-const postsRouter =require('./routes/posts')
-app.use("/posts",postsRouter)
-const postsLikesRouter=require('./routes/postsLikes')
-app.use("/postsLikes",postsLikesRouter)
-const postsCommentsRouter=require('./routes/postsComments')
-app.use("/postsComments",postsCommentsRouter)
-const imagesRouter = require ('./routes/images');
-app.use('/images', imagesRouter)
-
-db.sequelize.sync().then(()=>{
-    app.listen(5555,() => {
-        console.log ("Server running on PORT 5555")
-    })
-})
+db.sequelize.sync().then(() => {
+  app.listen(5555, () => {
+    console.log("Server running on PORT 5555");
+  });
+});
