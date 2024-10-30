f76bb93d83a511e768a891eb831538b523109b26
rua96 <rua96@hotmail.it>
2024-10-30 17:38:59 +0100

    30ooo

--- server/helpers/Validation.js
+++ server/helpers/Validation.js
@@ -1,19 +1,18 @@
-const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,30}$/
+const passwordRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+=[\]{}|\\:;"'<>,.?/~`-])[A-Za-z\d!@#$%^&*()_+=[\]{}|\\:;"'<>,.?/~`-]{12,30}$/;
 const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
-const usernameRegex= /^[A-Za-z\d]{4,12}$/;
+const usernameRegex = /^[A-Za-z\d]{4,12}$/;
 
 const Validation = {
-
-    isValidPassword(input) {
-        return input.match(passwordRegex)
-    },
-    isValidEmail(input) {
-        return input.match(emailRegex)
-    },
-    isValidUsername(input) {
-        return input.match(usernameRegex)
-    },
-
-}
+  isValidPassword(input) {
+    return input.match(passwordRegex);
+  },
+  isValidEmail(input) {
+    return input.match(emailRegex);
+  },
+  isValidUsername(input) {
+    return input.match(usernameRegex);
+  },
+};
 
 module.exports = Validation;
